68c05ae38a8ea17943df846a83d0d929152519ac
AMIRUL FARIS <149490355+mirulritory@users.noreply.github.com>
2025-08-19 22:59:42 +0800

    Update db.js
    Remove hardcoded database credentials

--- db.js
+++ db.js
@@ -21,11 +21,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'psm.postgres.database.azure.com',
-  database: process.env.DB_NAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'Nbn_1259',
-  port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   ssl: { rejectUnauthorized: false } // For cloud DBs like Azure
 });
 
